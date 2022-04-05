30af1f1024aeac2a217fac508c54b055bd1fb367
Hridoy <iamhridoyhaque.com>
2022-04-06 00:54:43 +0600

    complete calculator project

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
 window.onload = () => {
 
-    // SELECTION PART
+    // ================= SELECT DOM ELEMENTS ================= //
+
     const layout = document.querySelector('.center_div'),
           menu_btn = document.querySelector('.menu_btn'),
           bgColors = document.querySelector('.bgColors'),
@@ -13,7 +14,7 @@ window.onload = () => {
 
     })
 
-    // CHANGE CALCULATOR BACKGROUND COLOR
+    // ================= CHANGE CALCULATOR BACKGROUND COLOR ================= //
 
     if(bgColors){
         colors.forEach(item =>{
@@ -26,7 +27,8 @@ window.onload = () => {
         })
     }
 
-    // CHANGE COLOR FUNCTION
+    // ================= CHANGE CALCULATOR BACKGROUND COLOR FUNCTION ================= //
+
     function changeColor(item){
         if(item.classList.contains('blue')){
 
@@ -43,13 +45,17 @@ window.onload = () => {
         }
     }
 
-    // WORKING ON INPUT OR CALCULATION AREA
+    // ================= SELECT DOM ELEMENTS FOR FORM/CALCULATION ================= //
 
     const inp_btn = document.querySelector('.input_btn'),
           btns = document.querySelectorAll('.input_btn button'),
           input = document.getElementById('input_field'),
-          recent = document.getElementById('recent_result')
+          recent = document.getElementById('setResult'),
+          reset = document.querySelector('.reset')
+
     
+
+    // ================= SET FORM OPEARTION ================= //
 
     inp_btn.addEventListener('submit',function(e){
         e.preventDefault();
@@ -61,9 +67,9 @@ window.onload = () => {
         })
     })
 
-    //SET INPUT FIELD
-    let dot = 0
+    // ================= REACTION ON WHENT BUTTONS ARE CLICK ================= //
 
+    let dot = 0
 
     function setField(value){
         if(value === 'ac'){
@@ -71,8 +77,7 @@ window.onload = () => {
             dot = 0
         }else if(value === 'clear'){
             clearLast(input.value)
-        }else if(value === '%' || value === '/' || value === 'x' || value === '-'|| value === '+' ){
-
+        }else if(checkParamiter(value)){
             dot = 0
             setOperation(value)
         }else if( value === '='){
@@ -84,30 +89,48 @@ window.onload = () => {
         }
     }
 
+
+    // ================= FUNCTION FOR DELETE LAST ELEMENT  =================//
+
     function clearLast(value){
-        let val = value.slice(0,-1)
+        
+        let val = ''
+        if(value.charAt(value.length -1) === ' '){
+            val = value.slice(0,-3)
+        }else{
+            val = value.slice(0,-1)
+        }
         input.value = val
         if(input.value === ''){
             input.value = '0'
         }
     }
 
+
+    // ================= FUNCTION FOR OPEARTORS MOVEMENT ================= //
+
     function setOperation(value){
+        
         if(input.value.charAt(input.value.length-1) === '.'){
             input.value += '0'
         }
         let val = input.value
-        let len = val.length - 1
-
+        let len = val.length - 2
+        
         if(checkParamiter(val.charAt(len))){
-            val = val.slice(0,-1)
-            input.value = val + value
+            val = val.slice(0,-2)
+            input.value = ' '+ val + value + ' '
         }else{
-            input.value += value
+            input.value += ' ' + value + ' '
         }
-
+        if(value === '%'){
+            let percent = percentage()
+            input.value = percent.join(' ')
+        }
     }
 
+
+    // ================= FUNCITION FOR NUMBERS ================= //
 
     function setNumber(value){
         let len = input.value.length - 1
@@ -123,84 +146,68 @@ window.onload = () => {
             }else{
                 input.value = value
             }
-        }else if(input.value.charAt(len) === '%'){
-            input.value += 'x' + value
-        }
-        else{
+        }else{
             input.value += value
         }
-        
-        
     }
 
+
+    // ================= FUNCTION FOR REGULER CHECKING ================= //
+
     function checkParamiter(value){
-        if(value === '/' || value === 'x' || value === '-'|| value === '+'){
+        if(value === '%' || value === '/' || value === '*' || value === '-'|| value === '+' ){
             return true
         }
         return false
     }
 
+
+    // ================= FUNCTION FOR GET PERCENTAGE ================= //
+
+    function percentage(){
+        let arr = input.value.split(' ')
+        let index = arr.length
+        let perse = 0
+        if(index === 3){
+            perse = Number.parseFloat(arr[0]) / 100
+        }else{
+            perse = (Number.parseFloat(arr[index-5]) * Number.parseFloat(arr[index - 3])) / 100
+        }
+        arr.pop()
+        arr.pop()
+        arr.pop()
+        arr.push(perse.toString())
+        return arr
+    }
+
+
+    // ================= FUNCTION FOR SET RESULT ================= //
+
     function setResult(value){
-        let store = ''
-        let count = 0
-        let number = 0
-        let op = '+'
-        let ans = 0
-        for (let i = 0; i < value.length; i++) {
-            while((value.charAt(i) <= '9' && value.charAt(i) >= '0') || value.charAt(i) === '.' || value.charAt(i) === '%'){
-                store += value.charAt(i)
-                i++
+        let len = (value.split(/[+-/*]/)).length
+        
+        console.log(len)
+        if(len > 1){
+            if(checkParamiter(value.charAt(value.length-2))){
+                value = value.slice(0,-3)
             }
-
-            if(store.charAt(store.length - 1) === '%'){
-                count++
-                store = store.slice(0,-1)
-                if(ans === 0){
-                    number = Number.parseFloat(store) / 100
-                }else{
-                    number = ans * (Number.parseFloat(store)) / 100
-                }
-
-            }else{
-                number = Number.parseFloat(store)
-            }
-            
-
-            console.log(number +'ans -- ' + op)
-
-            if(op === '+'){
-                ans += number
-            }else if(op === '-'){
-                ans -= number
-            }else if(op === 'x'){
-                if(count !== 0){
-                    ans = number
-                }else{
-                    ans *= number
-                }
-            }else if(op === '/'){
-                ans /= number
-            }
-            
-            // if(op === '%'){
-            //         console.log(store + 'th')
-            // }
-            // else{
-            //     console.log(store + 'th')
-            // }
-
-            // console.log(number+" "+ans)
-            console.log(op)
-            console.log('ans = '+ans)
-            store = ''
-            number = 0
-            if(count === 0){
-                op = value.charAt(i)
-            }
-            
-            // while(value.charAt)
+            input.value = eval(value)
+            let newVal = input.value
+            recent.innerHTML += `${value} = <br> ${newVal} <br>`
+            reset.classList.add('active')
         }
         
     }
+
+
+    // ================= RESET INPUT FIELD ================= //
+
+    if(reset){
+        reset.addEventListener('click',function(){
+            recent.innerHTML = ''
+            this.classList.remove('active')
+        })
+    }
+
 
 }
